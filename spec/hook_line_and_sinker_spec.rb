ed8f6aaaa65cbfc27f74def383a24f3d284df0c7
Toby Clarke <teeohbee@gmail.com>
2016-06-08 21:26:12 +0100

    Add additional POST test and refactor

--- spec/hook_line_and_sinker_spec.rb
+++ spec/hook_line_and_sinker_spec.rb
@@ -2,12 +2,29 @@ require 'spec_helper'
 require 'json'
 
   describe 'Posting to the / route' do
-    it 'creates DB entries for incoming emails' do
-      email = {"Address":"barney@lostmy.name","EmailType":"Shipment","Event":"send","Timestamp":1432820696}
-      json = email.to_json
-      post_json('/', json)
+    good_email = {"Address":"barney@lostmy.name","EmailType":"Shipment","Event":"send","Timestamp":1432820696}.to_json
+    bad_email = {"Address":"barney@lostmy.name","EmailType":12345,"Event":"send","Timestamp":"This should be an int"}.to_json
+
+    it 'returns a successful status code and message when fields are valid' do
+      post_json('/', good_email)
       expect(last_response.status).to eql(200)
       expect(last_response.body.to_s).to eql('Webhook Data Parsed')
+    end
+
+    it 'returns a failing status code and message when fields are invalid or missing' do
+      post_json('/', bad_email)
+      expect(last_response.status).to eql(400)
+      expect(last_response.body.to_s).to eql('Data failed validations')
+    end
+
+    it 'adds a record to the email database when successful' do
+      post_json('/', good_email)
+      expect(Email.count).to eql(1)
+    end
+
+    it 'should correctly store model properties when successful' do
+      post_json('/', good_email)
+      expect(Email.first.address).to eql("barney@lostmy.name")
     end
   end
 
